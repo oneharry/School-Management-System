a02966d5e3b20421a8150caac1cc34b9b7b88046
ajeigbepaul <ajeigbepaul@yahoo.com>
2023-09-12 14:33:45 +0100

    worked on the about us

--- my-project/src/components/Header.jsx
+++ my-project/src/components/Header.jsx
@@ -1,21 +1,67 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-function Header() {
+function Header({handlePages, activepage}) {
+  let auth = true;
+  
   return (
-    <div className="w-full h-[10vh]">
-      <div className="w-3/5 mx-auto flex items-center justify-between py-2">
-        <div>LOGO</div>
-        <div className='flex items-center space-x-2'>
-          <div>About</div>
-          <div>features</div>
-          <div>Contact us</div>
+    <div className="w-full lg:h-[10vh] h-[20vh] mb-5 lg:mb-0">
+      <div className="lg:w-3/5 w-full mx-auto lg:flex items-center lg:justify-between justify-center py-2 ">
+        <div className="text-center">LOGO</div>
+        <div className="flex items-center space-x-5 justify-center">
+          <div
+            onClick={() => handlePages("home")}
+            className={`cursor-pointer ${
+              activepage === "home"
+                ? "border-b border-orange-400 text-orange-400"
+                : "  text-gray-700"
+            }`}
+          >
+            Home
+          </div>
+          <div
+            onClick={() => handlePages("about")}
+            className={`cursor-pointer ${
+              activepage === "about"
+                ? "border-b border-orange-400 text-orange-400"
+                : "  text-gray-700"
+            }`}
+          >
+            About
+          </div>
+          <div
+            className={`cursor-pointer ${
+              activepage === "features"
+                ? "border-b border-orange-400 text-orange-400"
+                : " text-gray-700"
+            }`}
+            onClick={() => handlePages("features")}
+          >
+            features
+          </div>
+          <div
+            className={`cursor-pointer ${
+              activepage === "contactus"
+                ? "border-b border-orange-400 text-orange-400"
+                : " text-gray-700"
+            }`}
+            onClick={() => handlePages("contactus")}
+          >
+            Contact us
+          </div>
         </div>
-        <div>
-            <div>Login/Register</div>
+        <div className="flex items-center justify-center">
+          {auth ? (
+            <button className="px-2 py-1 rounded-md bg-orange-300 text-xs text-white font-semibold">
+              Logout
+            </button>
+          ) : (
+            <Link to="">Login/Register</Link>
+          )}
         </div>
       </div>
     </div>
   );
 }
 
-export default Header
+export default Header;
