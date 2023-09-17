8b0b363dfa229192b5dc9bff095ffab7f094511e
ajeigbepaul <ajeigbepaul@yahoo.com>
2023-09-17 23:29:25 +0100

    pull the login and register

--- my-project/src/pages/login/Login.jsx
+++ my-project/src/pages/login/Login.jsx
@@ -2,28 +2,32 @@ import React from "react";
 import "./Login.css";
 function Login() {
   return (
-    <div className="container">
-      <h2 className="login">Login</h2>
-      <form>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" placeholder="Enter Username" />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" placeholder="Enter Password" />
-        </div>
-        <a href="#" className="forgot-password">
-          Forgot Password?
-        </a>
-        <button type="submit">Sign In</button>
+    <div className="w-full flex items-center bg-gray-100 flex-col h-screen">
+      <div className="w-1/4 bg-white rounded-lg p-4 flex flex-col mt-10">
+        <h2 className="login">Login</h2>
+        <form>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" placeholder="Enter Username" />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" placeholder="Enter Password" />
+          </div>
+          <a href="#" className="forgot-password">
+            Forgot Password?
+          </a>
+          <button type="submit" className="bg-orange-300">
+            Sign In
+          </button>
+        </form>
         <p>
           Don't have an account?{" "}
-          <a href="./register" className="sign-up">
+          <a href="./register" className="text-orange-400">
             Sign Up
           </a>
         </p>
-      </form>
+      </div>
     </div>
   );
 }
--- my-project/src/pages/register/Register.jsx
+++ my-project/src/pages/register/Register.jsx
@@ -4,25 +4,32 @@ import "./Register.css";
 
 function Login() {
   return (
-    <div className="login-container">
-      <h2 className="reg">Register</h2>
-      <form>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" placeholder="Enter Username" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email address:</label>
-          <input type="text" id="username" placeholder="Email" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" placeholder="Enter Password" />
-        </div>
-        <button type="submit">Sign me up</button>
-      </form>
-      <p>Already have an account? <a href="./login" className="sign-In">Sign In</a></p>
-    </div> 
+    <div className="w-full flex items-center bg-gray-100 flex-col h-screen">
+      <div className="w-1/4 bg-white rounded-lg p-4 flex flex-col mt-10">
+        <h2 className="reg">Register</h2>
+        <form>
+          <div className="">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" placeholder="Enter Username" />
+          </div>
+          <div className="">
+            <label htmlFor="email">Email address</label>
+            <input type="text" id="email" placeholder="Email" />
+          </div>
+          <div className="">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" placeholder="Enter Password" />
+          </div>
+          <button type="submit">Sign me up</button>
+        </form>
+      </div>
+      <p className="my-5">
+        Already have an account?{" "}
+        <a href="./login" className="text-orange-400">
+          Sign In
+        </a>
+      </p>
+    </div>
   );
 }
 
