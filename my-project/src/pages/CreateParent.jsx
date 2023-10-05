52c6215e4f902ac9d6d7a9a882cc6b80f1baad18
AdamaMuhammad <ndakoadama01@gmail.com>
2023-10-04 19:30:08 -0700

    update

--- my-project/src/pages/CreateParent.jsx
+++ my-project/src/pages/CreateParent.jsx
@@ -4,41 +4,61 @@ function CreateParent() {
   return (
     <div className="w-full flex items-center bg-gray-100 flex-col h-screen">
       <div className="lg:w-1/4 w-full bg-white rounded-lg p-4 flex flex-col mt-10">
-        <h2 className="login">Login</h2>
-        <form onSubmit={handleSubmit} className="w-full">
+        <h2 className="login">Parent Portal</h2>
+        <form className="w-full">
           <div className="w-full">
-            <label htmlFor="username">Email</label>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              placeholder="Parent Name"
+              // value={email}
+              // onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="w-full">
+            <label htmlFor="phone">Phone:</label>
+            <input
+              type="text"
+              id="phone"
+              placeholder="Parent Phone"
+              // value={password}
+              // onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="w-full">
+            <label htmlFor="email">Email:</label>
             <input
               type="text"
               id="email"
-              placeholder="Enter Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Parent email"
+              // value={password}
+              // onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <div className="w-full">
-            <label htmlFor="password">Password:</label>
+            <label htmlFor="id">ID:</label>
             <input
-              type="password"
-              id="password"
-              placeholder="Enter Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              type="text"
+              id="parent-id"
+              placeholder="Parent Id"
+              // value={password}
+              // onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <a href="#" className="forgot-password">
+          {/* <a href="#" className="forgot-password">
             Forgot Password?
-          </a>
+          </a> */}
           <button type="submit" className="bg-orange-300 w-full">
             Sign In
           </button>
         </form>
-        <p>
+        {/* <p>
           Don't have an account?{" "}
           <a href="./register" className="text-orange-400">
             Sign Up
           </a>
-        </p>
+        </p> */}
       </div>
     </div>
   );
